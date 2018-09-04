bfe996b28bd894c925019ed033884d87763fbc43
Antony Hanna <antonyhanna01@gmail.com>
2018-09-04 13:00:00 +1000

    Removed hardcoded formatting in place for automation
    Removed all hardcode placement code in an an attempt to come up with a simple and quick solution.

--- Symphony_Parser/Program.cs
+++ Symphony_Parser/Program.cs
@@ -3,6 +3,7 @@ using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 class Test
 {
@@ -24,16 +25,18 @@ class Test
 
         FileInfo outputFilePath = new FileInfo(outputDirectory);
 
-        ReadReport(className, filePath, outputFilePath, outputDirectory);
+        ReadReport(filePath, outputFilePath, outputDirectory);
 
-        Console.WriteLine("Your barcode report is now ready and available in {0}", outputDirectory);
+        Console.WriteLine("Your barcode report is now ready and available in:");
+        Console.WriteLine(outputDirectory);
+        Whitespace();
         Console.WriteLine("Press any key to end the program.");
         Whitespace();
 
         Console.Read();
     }
 
-    public static void ReadReport(string className, string reportPath, FileInfo outputDirectory, string outputString)
+    public static void ReadReport(string reportPath, FileInfo outputDirectory, string outputString)
     {
 
         List<string> usersList = new List<string>();
@@ -62,10 +65,10 @@ class Test
 
         }
 
-        writeToFile(outputDirectory, usersList, outputString);
+        WriteToFile(outputDirectory, usersList, outputString);
     }
 
-    public static void writeToFile(FileInfo outputDirectory, List<string> usersList, string outputString)
+    public static void WriteToFile(FileInfo outputDirectory, List<string> usersList, string outputString)
     {
         FileExists(outputDirectory, outputString);
         using (ExcelPackage excel = new ExcelPackage(outputDirectory))
@@ -80,154 +83,55 @@ class Test
             ws.Column(3).Width = 30.00;
             ws.Cells["A1:Z150"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
 
-            //---------------------------------------
 
-            ws.Cells["A2"].Style.Font.Size = 30;
-            ws.Cells["A2"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["A2"].Value = usersList[0];
-            ws.Cells["A3"].Value = usersList[1];
-            ws.Cells["A4"].Value = usersList[3];
-
-            ws.Cells["B2"].Style.Font.Size = 30;
-            ws.Cells["B2"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["B2"].Value = usersList[3];
-            ws.Cells["B3"].Value = usersList[4];
-            ws.Cells["B4"].Value = usersList[5];
-
-            ws.Cells["C2"].Style.Font.Size = 30;
-            ws.Cells["C2"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["C2"].Value = usersList[6];
-            ws.Cells["C3"].Value = usersList[7];
-            ws.Cells["C4"].Value = usersList[8];
-
-            //-----------------------------------------
-
-            ws.Cells["A7"].Style.Font.Size = 30;
-            ws.Cells["A7"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["A7"].Value = usersList[9];
-            ws.Cells["A8"].Value = usersList[10];
-            ws.Cells["A9"].Value = usersList[11];
-
-            ws.Cells["B7"].Style.Font.Size = 30;
-            ws.Cells["B7"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["B7"].Value = usersList[12];
-            ws.Cells["B8"].Value = usersList[13];
-            ws.Cells["B9"].Value = usersList[14];
-
-            ws.Cells["C7"].Style.Font.Size = 30;
-            ws.Cells["C7"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["C7"].Value = usersList[9];
-            ws.Cells["C8"].Value = usersList[10];
-            ws.Cells["C9"].Value = usersList[11];
-
-            //-----------------------------------------
-
-            ws.Cells["A12"].Style.Font.Size = 30;
-            ws.Cells["A12"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["A12"].Value = usersList[12];
-            ws.Cells["A13"].Value = usersList[13];
-            ws.Cells["A14"].Value = usersList[14];
-
-            ws.Cells["B12"].Style.Font.Size = 30;
-            ws.Cells["B12"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["B12"].Value = usersList[15];
-            ws.Cells["B13"].Value = usersList[16];
-            ws.Cells["B14"].Value = usersList[17];
-
-            ws.Cells["C12"].Style.Font.Size = 30;
-            ws.Cells["C12"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["C12"].Value = usersList[18];
-            ws.Cells["C13"].Value = usersList[19];
-            ws.Cells["C14"].Value = usersList[20];
-
-            // --------------------------------------
+            int column = 1; //Whats used as the row identifier
+            int columnCounter = 0;
+            int dataInt = 0;
+            int allowDown = 0;
 
 
-            ws.Cells["A17"].Style.Font.Size = 30;
-            ws.Cells["A17"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["A17"].Value = usersList[21];
-            ws.Cells["A18"].Value = usersList[22];
-            ws.Cells["A19"].Value = usersList[23];
+            for (int row = 1; row < usersList.Count;) //now increments downwards and sideways constantly
+            {
 
-            ws.Cells["B17"].Style.Font.Size = 30;
-            ws.Cells["B17"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["B17"].Value = usersList[24];
-            ws.Cells["B18"].Value = usersList[25];
-            ws.Cells["B19"].Value = usersList[26];
 
-            ws.Cells["C17"].Style.Font.Size = 30;
-            ws.Cells["C17"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["C17"].Value = usersList[27];
-            ws.Cells["C18"].Value = usersList[28];
-            ws.Cells["C19"].Value = usersList[29];
+                if (columnCounter >= 3 && row <= usersList.Count)
+                {
+                    
+                    column++;
+                    columnCounter = 0;
+                }
 
-            //---------------------------------------
+                if (allowDown >= 9 && row <= usersList.Count)
+                    {
+                        allowDown = 0;
+                        row++;
 
-            ws.Cells["A22"].Style.Font.Size = 30;
-            ws.Cells["A22"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["A22"].Value = usersList[30];
-            ws.Cells["A23"].Value = usersList[31];
-            ws.Cells["A24"].Value = usersList[32];
+                    }
 
-            ws.Cells["B22"].Style.Font.Size = 30;
-            ws.Cells["B22"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["B22"].Value = usersList[33];
-            ws.Cells["B23"].Value = usersList[34];
-            ws.Cells["B24"].Value = usersList[35];
 
-            ws.Cells["C22"].Style.Font.Size = 30;
-            ws.Cells["C22"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["C22"].Value = usersList[36];
-            ws.Cells["C23"].Value = usersList[37];
-            ws.Cells["C24"].Value = usersList[38];
+             
+                try
+                {
+                    ws.Cells[row, column].Value = usersList[dataInt];
+                }
 
-            //---------------------------------------
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            
 
-            ws.Cells["A25"].Style.Font.Size = 30;
-            ws.Cells["A25"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["A25"].Value = usersList[39];
-            ws.Cells["A26"].Value = usersList[40];
-            ws.Cells["A27"].Value = usersList[41];
+                allowDown++;
+                columnCounter++;
+                dataInt++;
 
-            ws.Cells["B25"].Style.Font.Size = 30;
-            ws.Cells["B25"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["B25"].Value = usersList[42];
-            ws.Cells["B26"].Value = usersList[43];
-            ws.Cells["B27"].Value = usersList[44];
+            }
 
-            ws.Cells["C25"].Style.Font.Size = 30;
-            ws.Cells["C25"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["C25"].Value = usersList[45];
-            ws.Cells["C26"].Value = usersList[46];
-            ws.Cells["C27"].Value = usersList[47];
-
-            //---------------------------------------
-
-            ws.Cells["A30"].Style.Font.Size = 30;
-            ws.Cells["A30"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["A30"].Value = usersList[48];
-            ws.Cells["A31"].Value = usersList[49];
-            ws.Cells["A32"].Value = usersList[50];
-
-            ws.Cells["B30"].Style.Font.Size = 30;
-            ws.Cells["B30"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["B30"].Value = usersList[51];
-            ws.Cells["B31"].Value = usersList[52];
-            ws.Cells["B32"].Value = usersList[53];
-
-            ws.Cells["C30"].Style.Font.Size = 30;
-            ws.Cells["C30"].Style.Font.Name = "Free 3 of 9 Extended";
-            ws.Cells["C30"].Value = usersList[54];
-            ws.Cells["C31"].Value = usersList[55];
-            ws.Cells["C32"].Value = usersList[56];
-
-            //---------------------------------------
+           
 
 
 
-
-
-            excel.Save();
+            excel.SaveAs(outputDirectory);
             Console.WriteLine("File has been saved to the specified directory.");
         }
     }
