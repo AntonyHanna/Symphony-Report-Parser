19e26da31adeb7a330a1cbfdd1e54e6f420679ce
Antony Hanna <antonyhanna01@gmail.com>
2018-09-15 12:44:36 +1000

    Added Symphony Parser GUI Project #8

---
+++ Symphony Parser GUI/Symphony Parser GUI/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Symphony_Parser_GUI_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileDirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitButton = new System.Windows.Forms.Button();
+            this.ReportDirectoryLabel = new System.Windows.Forms.Label();
+            this.reportDirectoryBrowseButton = new System.Windows.Forms.Button();
+            this.outputDirectoryBrowse = new System.Windows.Forms.Button();
+            this.outputDirectoryLabel = new System.Windows.Forms.Label();
+            this.outputDirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.gradeTextBox = new System.Windows.Forms.TextBox();
+            this.outputDirectoryErrorLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(110, 479);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(146, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Symphony Barcode Generator";
+            // 
+            // fileDirectoryTextBox
+            // 
+            this.fileDirectoryTextBox.Location = new System.Drawing.Point(124, 146);
+            this.fileDirectoryTextBox.Name = "fileDirectoryTextBox";
+            this.fileDirectoryTextBox.Size = new System.Drawing.Size(211, 20);
+            this.fileDirectoryTextBox.TabIndex = 1;
+            this.fileDirectoryTextBox.TextChanged += new System.EventHandler(this.fileDirectoryTextBox_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // exitButton
+            // 
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Location = new System.Drawing.Point(341, 444);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // ReportDirectoryLabel
+            // 
+            this.ReportDirectoryLabel.AutoSize = true;
+            this.ReportDirectoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReportDirectoryLabel.Location = new System.Drawing.Point(124, 127);
+            this.ReportDirectoryLabel.Name = "ReportDirectoryLabel";
+            this.ReportDirectoryLabel.Size = new System.Drawing.Size(98, 13);
+            this.ReportDirectoryLabel.TabIndex = 4;
+            this.ReportDirectoryLabel.Text = "Report Location";
+            // 
+            // reportDirectoryBrowseButton
+            // 
+            this.reportDirectoryBrowseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.reportDirectoryBrowseButton.Location = new System.Drawing.Point(341, 144);
+            this.reportDirectoryBrowseButton.Name = "reportDirectoryBrowseButton";
+            this.reportDirectoryBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.reportDirectoryBrowseButton.TabIndex = 5;
+            this.reportDirectoryBrowseButton.Text = "Browse";
+            this.reportDirectoryBrowseButton.UseVisualStyleBackColor = true;
+            this.reportDirectoryBrowseButton.Click += new System.EventHandler(this.reportDirectoryBrowseButton_Click);
+            // 
+            // outputDirectoryBrowse
+            // 
+            this.outputDirectoryBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.outputDirectoryBrowse.Location = new System.Drawing.Point(341, 216);
+            this.outputDirectoryBrowse.Name = "outputDirectoryBrowse";
+            this.outputDirectoryBrowse.Size = new System.Drawing.Size(75, 23);
+            this.outputDirectoryBrowse.TabIndex = 8;
+            this.outputDirectoryBrowse.Text = "Browse";
+            this.outputDirectoryBrowse.UseVisualStyleBackColor = true;
+            this.outputDirectoryBrowse.Click += new System.EventHandler(this.outputDirectoryBrowse_Click);
+            // 
+            // outputDirectoryLabel
+            // 
+            this.outputDirectoryLabel.AutoSize = true;
+            this.outputDirectoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputDirectoryLabel.Location = new System.Drawing.Point(124, 199);
+            this.outputDirectoryLabel.Name = "outputDirectoryLabel";
+            this.outputDirectoryLabel.Size = new System.Drawing.Size(98, 13);
+            this.outputDirectoryLabel.TabIndex = 7;
+            this.outputDirectoryLabel.Text = "Output Location";
+            // 
+            // outputDirectoryTextBox
+            // 
+            this.outputDirectoryTextBox.Location = new System.Drawing.Point(124, 218);
+            this.outputDirectoryTextBox.Name = "outputDirectoryTextBox";
+            this.outputDirectoryTextBox.Size = new System.Drawing.Size(211, 20);
+            this.outputDirectoryTextBox.TabIndex = 6;
+            this.outputDirectoryTextBox.TextChanged += new System.EventHandler(this.outputDirectoryTextBox_TextChanged);
+            // 
+            // generateButton
+            // 
+            this.generateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.generateButton.Location = new System.Drawing.Point(116, 444);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(129, 23);
+            this.generateButton.TabIndex = 9;
+            this.generateButton.Text = "Generate Barcodes";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.AutoSize = true;
+            this.gradeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeLabel.Location = new System.Drawing.Point(124, 56);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(37, 13);
+            this.gradeLabel.TabIndex = 11;
+            this.gradeLabel.Text = "Class";
+            // 
+            // gradeTextBox
+            // 
+            this.gradeTextBox.Location = new System.Drawing.Point(124, 75);
+            this.gradeTextBox.Name = "gradeTextBox";
+            this.gradeTextBox.Size = new System.Drawing.Size(120, 20);
+            this.gradeTextBox.TabIndex = 10;
+            // 
+            // outputDirectoryErrorLabel
+            // 
+            this.outputDirectoryErrorLabel.AutoSize = true;
+            this.outputDirectoryErrorLabel.Location = new System.Drawing.Point(124, 241);
+            this.outputDirectoryErrorLabel.Name = "outputDirectoryErrorLabel";
+            this.outputDirectoryErrorLabel.Size = new System.Drawing.Size(10, 13);
+            this.outputDirectoryErrorLabel.TabIndex = 13;
+            this.outputDirectoryErrorLabel.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(428, 479);
+            this.Controls.Add(this.outputDirectoryErrorLabel);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.gradeTextBox);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.outputDirectoryBrowse);
+            this.Controls.Add(this.outputDirectoryLabel);
+            this.Controls.Add(this.outputDirectoryTextBox);
+            this.Controls.Add(this.reportDirectoryBrowseButton);
+            this.Controls.Add(this.ReportDirectoryLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.fileDirectoryTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileDirectoryTextBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label ReportDirectoryLabel;
+        private System.Windows.Forms.Button reportDirectoryBrowseButton;
+        private System.Windows.Forms.Button outputDirectoryBrowse;
+        private System.Windows.Forms.Label outputDirectoryLabel;
+        private System.Windows.Forms.TextBox outputDirectoryTextBox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Label gradeLabel;
+        private System.Windows.Forms.TextBox gradeTextBox;
+
+        private System.Windows.Forms.Label outputDirectoryErrorLabel;
+    }
+}
+
---
+++ Symphony Parser GUI/Symphony Parser GUI/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+
+namespace Symphony_Parser_GUI_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void reportDirectoryBrowseButton_Click(object sender, EventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text files (*.txt) |*.txt";
+
+            Nullable<bool> result = dialog.ShowDialog();
+
+            if(result == true)
+            {
+                string fileName = dialog.FileName;
+                fileDirectoryTextBox.Text = fileName;
+            }
+        }
+
+        private void fileDirectoryTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputDirectoryBrowse_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string folderPath = fbd.SelectedPath + @"\";
+                    outputDirectoryTextBox.Text = fbd.SelectedPath + @"\";
+                }
+            }   
+        }
+
+        private void outputDirectoryTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            string className = gradeTextBox.Text;
+            string filePath = fileDirectoryTextBox.Text;
+            string outputString = outputDirectoryTextBox.Text;
+            string outputDirectory = outputString + className + " - " + DateTime.Now.ToString("dddd, dd MMMM yyyy") + ".xlsx";
+            FileInfo outputFilePath = new FileInfo(outputDirectory);
+            Start(filePath, outputFilePath, outputDirectory, className, outputDirectoryErrorLabel);
+        }
+
+        static void Start(string filePath, FileInfo outputFilePath, string outputDirectory, string className, Label outputDirectoryErrorLabel)
+        {
+            ReadReport(filePath, outputFilePath, outputDirectory, className, outputDirectoryErrorLabel);
+        }
+
+        public static void ReadReport(string reportPath, FileInfo outputDirectory, string outputString, string className, Label outputDirectoryErrorLabel)
+        {
+            List<string> usersList = new List<string>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(reportPath))
+                {
+                    string[] allLine = File.ReadAllLines(reportPath);
+
+                    foreach (string unsplitUser in allLine)
+                    {
+                        if (unsplitUser.Contains("|"))
+                        {
+                            string[] splitUser = unsplitUser.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+                            foreach (string data in splitUser)
+                            {
+                                string cleanedString = System.Text.RegularExpressions.Regex.Replace(data, @"\s{2,}", "");
+                                usersList.Add(cleanedString);
+                                Console.WriteLine(data);
+                            }
+                        }
+                    }
+                    Console.WriteLine("Data has been read!");
+                }
+                FileExists(outputDirectory, outputString);
+                WriteToFile(outputDirectory, usersList, className);
+            }
+
+            catch(ArgumentException ex)
+            {
+                MessageBox.Show("Output Location cannot be left empty!");
+                outputDirectoryErrorLabel.Text = "";
+                return;
+            }
+            
+        }
+
+        public static void WriteToFile(FileInfo outputDirectory, List<string> usersList, string className)
+        {
+            using (ExcelPackage excel = new ExcelPackage(outputDirectory))
+            {
+                Console.WriteLine("File is being created.");
+                var ws = excel.Workbook.Worksheets.Add("Barcode Report");
+                Console.WriteLine("Users are being written to file.");
+
+                //Page Formatting
+                ws.Column(1).Width = 26.50;
+                ws.Column(2).Width = 26.50;
+                ws.Column(3).Width = 26.50;
+                ws.Cells["A:Z"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                //Function for Automatically writing data to an excel file
+                int idRow = 1; //Row in which the user ID is contained
+                int rowCounter = 1; //Keeps track of how many times the row has been incremented
+                int colCounter = 1; ////Keeps track of how many times the column has been incremented
+                int Column = 1; //Current Column
+                int Row = 1; //Current Row
+
+                foreach (string data in usersList)
+                {
+                    if (rowCounter == 4)
+                    {
+                        Row = Row - 3;
+                        rowCounter = 1;
+                        Column++;
+                        colCounter++;
+                    }
+
+                    if (colCounter == 4)
+                    {
+                        colCounter = 1;
+                        Column = 1;
+                        Row = Row + 4;
+                    }
+
+                    ws.Row(idRow).Style.Font.Size = 30;
+                    ws.Row(idRow).Style.Font.Name = "Free 3 of 9 Extended";
+                    ws.Cells[Row, Column].Value = data;
+                    Row++;
+                    rowCounter++;
+                    idRow = idRow + 4;
+                }
+                Whitespace();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0} user(s) written to file.", usersList.Count / 3);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                HeaderContentGap(ws, usersList);
+                fitPageBreakToPage(ws, usersList);
+                AddPageHeaders(ws, className);
+                WorkSheetProperties(ws);
+
+                excel.SaveAs(outputDirectory);
+            }
+        }
+
+        static void fitPageBreakToPage(ExcelWorksheet ws, List<string> userList)
+        {
+            //Remove all Page Breaks
+            for (int i = 1; i <= userList.Count + userList.Count / 3; i++)
+            {
+                ws.Row(i).PageBreak = false;
+                ws.Column(i).PageBreak = false;
+            }
+
+            //Create a page break at the end of every page
+            //Delete the Blank row (i) as it creates an empty row
+            //WARNING: If anything is resized then you'll likely have to update these values to fit everything again
+            for (int i = 40; i <= userList.Count + userList.Count / 3; i += 40)
+            {
+                ws.Row(i).PageBreak = true;
+                ws.DeleteRow(i);
+            }
+
+            ws.Column(3).PageBreak = true;
+        }
+
+        static void AddPageHeaders(ExcelWorksheet ws, string className)
+        {
+            ws.HeaderFooter.AlignWithMargins = true;
+            ws.HeaderFooter.EvenHeader.CenteredText = "&28&\"Arial,Regular Bold\"" + className + " - Barcodes";
+            ws.HeaderFooter.OddHeader.CenteredText = "&28&\"Arial,Regular Bold\"" + className + " - Barcodes";
+        }
+
+        static void HeaderContentGap(ExcelWorksheet ws, List<string> userList)
+        {
+            for (int i = 1; i <= userList.Count + userList.Count / 3; i += 40)
+            {
+                ws.InsertRow(i, 1);
+            }
+        }
+
+        static void WorkSheetProperties(ExcelWorksheet ws)
+        {
+            ws.View.PageLayoutView = true;
+        }
+
+        public static void FileExists(FileInfo outputDirectory, string outputString)
+        {
+            //Needs to be Modified to work with WPF
+            if (outputDirectory.Exists)
+            {
+                try
+                {
+                    File.Delete(outputString);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Please close the the Worksheet before trying to run this program.");
+                    Whitespace();
+                    Console.WriteLine("Press any key to exit!");
+                    Console.Read();
+                    Environment.Exit(1);
+                }
+            }
+        }
+
+        public static void Whitespace()
+        {
+            Console.WriteLine();
+        }
+
+        public static void ClearScreen()
+        {
+            Console.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
